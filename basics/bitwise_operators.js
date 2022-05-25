8387e66342c3a342da4f9f70634945302166e4b4
Srikanth Goshikonda <srikanthgoshikonda@gmail.com>
2022-05-25 11:23:18 +0530

    logical and bitwise operators

---
+++ basics/bitwise_operators.js
@@ -0,0 +1,35 @@
+//JavaScript Bitwise Operators
+let a =5 //Binary -> 101
+let b =6 //Binary -> 110
+
+//Bitwise AND
+console.log(a & b) //4
+
+//Bitwise OR
+console.log(a | b) //7
+
+//Bitwise XOR
+console.log(a ^ b) //3
+
+//Bitwise NOT
+console.log( ~b) //2's complement -> -7
+
+//Bitwise LEFT SHIFT
+console.log(a << b) //320
+
+//Bitwise RIGHT SHIFT
+console.log(a >> b) //0
+
+//Bitwise Zero-fill right shift
+console.log(a >>> b) //0
+
+
+//Other JavaScript Operators
+// Operator   
+// ,
+// ? :
+// delete
+// typeof
+// void
+// in
+// instanceof
---
+++ basics/logical_operators.js
@@ -0,0 +1,16 @@
+//JavaScript Logical Operators
+
+// logical AND
+console.log(true && true); // true
+console.log(true && false); // false
+console.log(true && null); // null
+
+// logical OR
+console.log(true || false); // true
+console.log('srikanth' || false); // srikanth
+
+// logical NOT
+console.log(!true); // false
+console.log(!85); // false
+
+
