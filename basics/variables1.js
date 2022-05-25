ec1f073ef09cb1a6209d4a77289f6e1899ede215
Srikanth Goshikonda <srikanthgoshikonda@gmail.com>
2022-05-25 08:15:50 +0530

    Variable Naming follow rules

---
+++ basics/variables1.js
@@ -0,0 +1,12 @@
+// Variable names follow these rules:
+
+// Variable names are case-sensitive. This means that the message and Message are different variables.
+// Variable names can only contain letters, numbers, underscores, or dollar signs and cannot contain spaces. Also, variable names must begin with a letter, an underscore (_) or a dollar sign ($).
+// Variable names cannot use the reserved words.
+
+let age = 25
+let mobile_number = '9876543210'
+let $currency = 1000
+let age1 = 10
+let _age = 15
+// let 1personAge = 15 Syntax Error
