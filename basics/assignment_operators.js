6153e509f2b2ae4795d942748d1e45f331c0b6d7
Srikanth Goshikonda <srikanthgoshikonda@gmail.com>
2022-05-25 11:02:15 +0530

    assignment and relational operators

---
+++ basics/assignment_operators.js
@@ -0,0 +1,40 @@
+// JavaScript Assignment Operators
+
+//Assignment operator
+let x = 5;
+let y = 3;
+let result;
+
+//Addition and assign operator
+result =x +=y
+console.log(result) //x=x+y:8
+
+//Subtract and assign operator
+x=5
+y=3
+result = x-=y
+console.log(result) //x=x-y:2
+
+//Multiply and assign operator
+x=5
+y=3
+result = x*=y
+console.log(result) //x=x*y:15
+
+//Divide and assign operator
+x=5
+y=3
+result = x/=y
+console.log(result) //x=x/y:1.6666
+
+//Modules and assign operator
+x=5
+y=3
+result  = x%=y
+console.log(result) //x=x%y:2
+
+//Exponent and assign operator
+x=2
+y=4
+result  = x**=y
+console.log(result) //x=x**y:16
---
+++ basics/relational_operators.js
@@ -0,0 +1,35 @@
+//JavaScript Comparison Operators
+let a =5
+let b =10
+let c= '5'
+let d= '10'
+
+//Equals to Operator
+console.log(a==b) //false
+
+//Not Equals to Operator
+console.log(a!=b) //true
+
+
+//Greater than Operator
+console.log(a>b) //false
+
+
+//Less than Operator
+console.log(a<b) //true
+
+
+//Greater than or equals to Operator
+console.log(a>=b) //false
+
+
+//Less than or equals to Operator
+console.log(a<=b) //true
+
+
+//Strict equals to Operator
+console.log(a===c) //false becoz types are not same
+
+
+//Strict not equals to Operator
+console.log(a!==5) //false becoz types are not same
